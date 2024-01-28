41410c590dc2d5c6f2823b6cc446f72a15529946
KangRok Yoon <ykr0919@naver.com>
2024-01-28 13:10:35 +0900

    first commit

---
+++ YU-LikeLion-iOS/YU_LikeLion_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  YU_LikeLion_iOSApp.swift
+//  YU-LikeLion-iOS
+//
+//  Created by 윤강록 on 1/28/24.
+//
+
+import SwiftUI
+
+@main
+struct YU_LikeLion_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YU-LikeLion-iOSUITests/YU_LikeLion_iOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  YU_LikeLion_iOSUITestsLaunchTests.swift
+//  YU-LikeLion-iOSUITests
+//
+//  Created by 윤강록 on 1/28/24.
+//
+
+import XCTest
+
+final class YU_LikeLion_iOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
